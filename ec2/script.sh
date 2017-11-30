7f036949611c1893e676d976c18802de6affd217
Tejas Taunad H V <tejastaunad@gmail.com>
2017-11-30 13:23:18 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.82/bin/apache-tomcat-7.0.82.tar.gz
 sudo tar -xvzf apache-tomcat-7.0.82.tar.gz
 sudo ./apache-tomcat-7.0.82/bin/startup.sh

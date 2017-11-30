ab33c29adbc31be08d0f88d9515a38bc4b28ce29
Tejas Taunad H V <tejastaunad@gmail.com>
2017-11-30 13:21:28 +0530

    Create script.sh

---
+++ ec2/script.sh
@@ -0,0 +1,3 @@
+sudo wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.82/bin/apache-tomcat-7.0.82.tar.gz
+sudo tar -xvzf apache-tomcat-7.0.82.tar.gz
+sudo ./apache-tomcat-7.0.82/bin/startup.sh

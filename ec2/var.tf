cddc41797252b14150a103834bd0878b77b46930
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-05 11:40:13 +0530

    Update var.tf

--- ec2/var.tf
+++ ec2/var.tf
@@ -5,3 +5,14 @@ variable "AMI"{
 variable "SG_IDs"{
 	default = ["sg-afb5cfd2"]
 }
+variable "remote_state_bucket"{
+	default = "jenkinstejas"
+}
+}
+variable "vpc_state_file_name"{
+	default = "vpc_state_file_name"
+}
+}
+variable "aws_region"{
+	default = "us-west-2"
+}

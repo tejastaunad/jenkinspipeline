788ec6776c27f25e02cf9f69656892f70fc7f56e
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-05 11:45:10 +0530

    Update script.sh
    Adding webpage code clone commands

--- ec2/script.sh
+++ ec2/script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#to install tomcat
 sudo wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.82/bin/apache-tomcat-7.0.82.tar.gz
 sudo tar -xvzf apache-tomcat-7.0.82.tar.gz
+#to git clone webpage code and launch it
+sudo cd apache-tomcat-7.0.82/webapps/
+sudo git clone https://github.com/tejastaunad/webjsp_for_jenkins.git
 sudo ./apache-tomcat-7.0.82/bin/startup.sh

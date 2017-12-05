151d60762bdf976ec61eec6d0463a234d83d6092
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-05 12:09:54 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# installing git
+sudo yum update
+sudo yum upgrade
+sudo yum install git -y
 #to install tomcat
 wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.82/bin/apache-tomcat-7.0.82.tar.gz
 tar -xvzf apache-tomcat-7.0.82.tar.gz

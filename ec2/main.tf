56099f1a8a9bec743faeaa215d6be986b5869e71
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-06 15:47:12 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,25 +1,18 @@
 provider "aws" {
   region = "us-west-2"
  }
-resource "terraform_remote_state" "vpc" {
-
+data "terraform_remote_state" "vpc" {
   backend = "s3"
-
   config {
-
     bucket = "${var.remote_state_bucket}"
-
     key = "${var.vpc_state_file_name}"
-
     region = "${var.aws_region}"
-
   }
-
 }
 resource "aws_instance" "Tejas_EC2" {
   ami           = "${var.AMI}"
   instance_type = "t2.micro"
-  subnet_id = "${terraform_remote_state.vpc.Pubsubnet_id}"
+  subnet_id = "${data.terraform_remote_state.vpc.Pubsubnet_id}"
   security_groups = "${var.SG_IDs}"
   key_name = "Tejas_keypair"
   availability_zone = "us-west-2a"

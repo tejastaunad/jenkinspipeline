47a94b9b09f8855c525d83679b19cd9e12322557
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-06 14:36:24 +0530

    Update var.tf

--- vpc/var.tf
+++ vpc/var.tf
@@ -30,10 +30,8 @@ variable "myip"{
 variable "remote_state_bucket"{
 	default = "jenkinstejas"
 }
-}
 variable "vpc_state_file_name"{
 	default = "ec2_state_file_name"
-}
 }
 variable "aws_region"{
 	default = "us-west-2"

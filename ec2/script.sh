0de980e66d685d2df4f0c49aa08148bc7383f6d7
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-06 16:47:42 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -7,7 +7,7 @@ sudo yum install git -y
 wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.82/bin/apache-tomcat-7.0.82.tar.gz
 tar -xvzf apache-tomcat-7.0.82.tar.gz
 #to git clone webpage code and launch it
-cd /home/ec2-user/apache-tomcat-7.0.82/webapps
+cd /apache-tomcat-7.0.82/webapps
 git clone https://github.com/tejastaunad/webjsp_for_jenkins.git
 cd
 ./apache-tomcat-7.0.82/bin/startup.sh

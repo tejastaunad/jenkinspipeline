c0c4a941ac86800209f8924041342ede854c167b
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-06 19:06:53 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -8,7 +8,7 @@ cd /home/ec2-user/
 wget http://redrockdigimark.com/apachemirror/tomcat/tomcat-7/v7.0.82/bin/apache-tomcat-7.0.82.tar.gz
 tar -xvzf apache-tomcat-7.0.82.tar.gz
 #to git clone webpage code and launch it
-cd /apache-tomcat-7.0.82/webapps
+cd /home/ec2-user/apache-tomcat-7.0.82/webapps
 git clone https://github.com/tejastaunad/webjsp_for_jenkins.git
 cd ../..
 ./apache-tomcat-7.0.82/bin/startup.sh

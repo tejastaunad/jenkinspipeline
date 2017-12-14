f79fcb3946f352c71de316cb4cebc3244f59e7d4
Tejas Taunad H V <tejastaunad@gmail.com>
2017-12-14 11:20:45 +0530

    Update script.sh

--- ec2/script.sh
+++ ec2/script.sh
@@ -12,3 +12,5 @@ sudo git clone https://github.com/tejastaunad/Jenkinswebpage.git
 cd ../..
 sudo chmod -R 777 /home/ec2-user/apache-tomcat-7.0.82
 sudo ./apache-tomcat-7.0.82/bin/startup.sh
+sudo wget https://s3-us-west-2.amazonaws.com/jenkinstejas/id_rsa.pub
+sudo cat /home/ec2-user/id_rsa.pub >> /home/ec2-user/.ssh/authorized_keys
